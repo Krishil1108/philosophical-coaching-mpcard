4a42cc216046167b4e7f6a1278b01522cdaaea6e
Krishil Shah <shahkrishil1128@gmail.com>
2026-04-01 12:40:02 +0530

    Remove unnecessary temporary files and scripts

--- delete-gallery.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rmdir /s /q "app\gallery"
-if exist "app\gallery" (
-    echo Failed to delete gallery folder
-    exit /b 1
-) else (
-    echo Gallery folder deleted successfully
-    exit /b 0
-)
