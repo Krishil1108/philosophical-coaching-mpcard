5ba7d5338acc6259f3bfc352ec4e324d77484743
Krishil Shah <shahkrishil1128@gmail.com>
2026-05-27 23:19:01 +0530

    ui: remove film grain background

--- app/components/UIEnhancements.tsx
+++ app/components/UIEnhancements.tsx
@@ -51,23 +51,7 @@ export function UIEnhancements() {
         }}
       />
 
-      {/* 2. Tactile Canvas Film Grain (Zero Network Load) */}
-      <div
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          pointerEvents: "none",
-          zIndex: 9998,
-          opacity: 0.45,
-          mixBlendMode: "multiply",
-          backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.8' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
-        }}
-      />
-
-      {/* 3. Ambient Reading Glow tracking the cursor */}
+      {/* 2. Ambient Reading Glow tracking the cursor */}
       <motion.div
         animate={{
           x: mousePosition.x - 300,
